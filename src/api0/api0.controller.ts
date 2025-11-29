50595dc9803471454e152e78792bb2f755eaeeba
Giorgi Mirianashvili <giorgimirianashvili27@gmail.com>
2025-11-29 16:54:39 +0400

    small patch

--- src/api0/api0.controller.ts
+++ src/api0/api0.controller.ts
@@ -1,10 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { Api0Service } from './api0.service';
 
 @Controller('api0')
 export class Api0Controller {
   constructor(private readonly api0Service: Api0Service) {
   }
+  @Get()
   getApi1(){
     return this.api0Service.getApi0();
   }
